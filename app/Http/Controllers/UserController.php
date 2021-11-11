26222a62913296d45a251119607f66dc82c9821d
Hand Wu <handwu@xwfintech.com>
2021-11-11 17:41:02 +0800

    使用laravel框架重新初始化项目

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Routing\Controller as BaseController;
+
+class UserController extends BaseController
+{
+    public function index() {
+        return "index";
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/greeting', function () {
+    return "hello world";
+});
+
+Route::get('/user', [\App\Http\Controllers\UserController::class, 'index']);
+
+Route::match(['get', 'post'], '/match', function () {
+    return "match";
+});
+
+Route::any('/any', function () {
+    return "any";
+});
